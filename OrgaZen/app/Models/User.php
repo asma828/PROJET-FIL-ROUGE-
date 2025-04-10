7a5f46e1919fded82240b698b39e9afe0a3406d8
asma828 <asmalachhab66@gmail.com>
2025-04-10 14:29:40 +0100

    Add role and business_name and event_category_id to fillable

--- OrgaZen/app/Models/User.php
+++ OrgaZen/app/Models/User.php
@@ -11,16 +11,24 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+    
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'role_id',
+        'business_name',
+        'event_category_id',
     ];
 
     /**
