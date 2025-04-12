354210f033d830ae4f6abbbb57ab3d3ae01efd31
asma828 <asmalachhab66@gmail.com>
2025-04-12 18:45:01 +0100

    bind the event category interface with repository

--- OrgaZen/app/Providers/AppServiceProvider.php
+++ OrgaZen/app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\EventCategory;
 use App\Repositories\AuthRepository;
+use App\Repositories\EventCategoryRepo;
 use App\Repositories\Interfaces\AuthRepositoryInterface;
+use App\Repositories\Interfaces\EventCategoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);    }
+        $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);  
+        $this->app->bind(EventCategoryInterface::class,EventCategoryRepo::class);
+    }
 
     /**
      * Bootstrap any application services.
