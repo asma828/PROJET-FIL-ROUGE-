5acb74846b8f979b0bbf9a29cc64663f8a6c3ab5
asma828 <asmalachhab66@gmail.com>
2025-04-12 18:46:45 +0100

    create the event categories controller with methode to display the categories

---
+++ OrgaZen/app/Http/Controllers/EventCategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EventCategory;
+use App\Repositories\Interfaces\EventCategoryInterface;
+use Illuminate\Http\Request;
+
+class EventCategoryController extends Controller
+{
+    protected $evenCategoryRepo;
+     public function __construct(EventCategoryInterface $evenCategoryRepo) {
+        $this->evenCategoryRepo= $evenCategoryRepo;
+    }
+
+    public function category(){
+        $categories=$this->evenCategoryRepo->getAllCategory();
+        return view('components.admin.CategoryManagement',compact('categories'));
+    }
+    public function store(Request $request){
+    $request->validate([
+        'name' => 'required|string',
+    ]);
+    
+}
+}
