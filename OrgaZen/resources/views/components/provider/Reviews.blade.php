3a77a4f5ec1ae1c0c3abe82edb19fa096cc9b3e0
asma828 <asmalachhab66@gmail.com>
2025-04-17 15:18:03 +0100

    fix a problem in logout form in provider pages

--- OrgaZen/resources/views/components/provider/Reviews.blade.php
+++ OrgaZen/resources/views/components/provider/Reviews.blade.php
@@ -89,7 +89,7 @@
                     @csrf
                     <i class="fas fa-sign-out-alt w-5 h-5 mr-3"></i>
                     <button>Logout</button>
-                </a>
+                </form>
             </div>
         </div>
         
