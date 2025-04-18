424c71dc852fb0b93f83b50af44cad8577a66f00
asma828 <asmalachhab66@gmail.com>
2025-04-18 15:36:47 +0100

    add meesing column to data array

--- OrgaZen/app/Repositories/UserRepository.php
+++ OrgaZen/app/Repositories/UserRepository.php
@@ -7,7 +7,7 @@ use Auth;
 class UserRepository implements UserInterface{
         public function getAuthenticatedUserProfile()
         {
-            return Auth::user()->load(['tags', 'service']);
+            return Auth::user()->load(['tags', 'services']);
         }
     
      public function updateProfile(array $data){
@@ -16,9 +16,15 @@ class UserRepository implements UserInterface{
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
-            'city' => $data['city'],
             'image' => $data['image'] ?? $user->image,
         ]);
+        if (isset($data['service_area'],$data['description'])) {
+            $user->services()->update([
+                'service_area' => $data['service_area'],
+                'description' => $data['description'],
+            ]);
+        }
+        
         if (isset($data['tags'])) {
             $user->tags()->sync($data['tags']);
         }
