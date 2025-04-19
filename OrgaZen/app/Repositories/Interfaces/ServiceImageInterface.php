50d0a961d3ee90db37f5bd704d2aebda78bc25a3
asma828 <asmalachhab66@gmail.com>
2025-04-19 23:28:39 +0100

    add getbyservice function to service images interface

--- OrgaZen/app/Repositories/Interfaces/ServiceImageInterface.php
+++ OrgaZen/app/Repositories/Interfaces/ServiceImageInterface.php
@@ -3,4 +3,6 @@ namespace App\Repositories\Interfaces;
 interface ServiceImageInterface {
     public function store($serviceId, $images);
     public function delete($id);
+    public function getByService($serviceId);
+
 }
