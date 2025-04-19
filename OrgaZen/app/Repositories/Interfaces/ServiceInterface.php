93639d072e2a89773efb19458b2d9ddb54a69671
asma828 <asmalachhab66@gmail.com>
2025-04-19 15:42:16 +0100

    add function for update and other one for displaying service for specefique provider

--- OrgaZen/app/Repositories/Interfaces/ServiceInterface.php
+++ OrgaZen/app/Repositories/Interfaces/ServiceInterface.php
@@ -5,4 +5,6 @@ namespace App\Repositories\Interfaces;
 interface ServiceInterface
 {
     public function store(array $data);
+    public function update($id, array $data);
+    public function find($id);
 }
