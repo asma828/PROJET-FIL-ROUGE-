9dca422780530616eb58d8275377c9a923359dea
asma828 <asmalachhab66@gmail.com>
2025-04-19 23:29:41 +0100

    implement the getbyservice function

--- OrgaZen/app/Repositories/ServiceImageRepository.php
+++ OrgaZen/app/Repositories/ServiceImageRepository.php
@@ -1,8 +1,28 @@
 <?php
 namespace App\Repositories;
 
+use App\Models\ServiceImage;
 use App\Repositories\Interfaces\ServiceImageInterface;
+use Storage;
 class ServiceImageRepository implements ServiceImageInterface{
-public function store($serviceId, $images){}
-public function delete($id){}
+
+    public function getByService($serviceId)
+    {
+        return ServiceImage::where('service_id', $serviceId)->get();
+    }
+public function store($serviceId, $images){
+    foreach ($images as $image) {
+        $path = $image->store('service_images', 'public');
+        ServiceImage::create([
+            'service_id' => $serviceId,
+            'image_path' => $path,
+        ]);
+    }
+}
+public function delete($id){
+    $image = ServiceImage::findOrFail($id);
+    Storage::disk('public')->delete($image->image_path);
+    $image->delete();
+
+}
 }
