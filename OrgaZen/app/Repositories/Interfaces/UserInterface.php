f487bed51804362cf4773971f608048cdbd724d1
asma828 <asmalachhab66@gmail.com>
2025-04-19 12:14:20 +0100

    create a function in user interface to get all providers

--- OrgaZen/app/Repositories/Interfaces/UserInterface.php
+++ OrgaZen/app/Repositories/Interfaces/UserInterface.php
@@ -4,4 +4,5 @@ namespace App\Repositories\Interfaces;
 interface UserInterface{
     public function getAuthenticatedUserProfile();
     public function updateProfile(array $data);
+    public function getAllProviders();
 }
