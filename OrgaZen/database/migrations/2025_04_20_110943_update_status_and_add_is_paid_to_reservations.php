1bc767f17ea73bcb151f9e8846fb34f32ef7b11e
asma828 <asmalachhab66@gmail.com>
2025-04-21 14:45:01 +0100

    add status column to reservation table

---
+++ OrgaZen/database/migrations/2025_04_20_110943_update_status_and_add_is_paid_to_reservations.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('reservation', function (Blueprint $table) {
+            $table->dropColumn('status');
+            $table->enum('status', ['step1', 'step2', 'step3', 'completed'])->default('step1');
+            $table->boolean('is_paid')->default(false);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('reservation', function (Blueprint $table) {
+            $table->dropColumn(['status', 'is_paid']);
+            $table->string('status')->default('draft');
+        });
+    }
+};
