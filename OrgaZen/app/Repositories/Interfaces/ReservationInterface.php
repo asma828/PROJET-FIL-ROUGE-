8a16f6eef937bbb89ec298b119a9cae5c8fe5af1
asma828 <asmalachhab66@gmail.com>
2025-04-21 23:07:04 +0100

    create the reservation interface with store and find functions

--- OrgaZen/app/Repositories/Interfaces/ReservationInterface.php
+++ OrgaZen/app/Repositories/Interfaces/ReservationInterface.php
@@ -4,8 +4,5 @@ namespace App\Repositories\Interfaces;
 interface ReservationInterface
 {
     public function store(array $data);
-    public function updateStep2($id, array $data);
-    public function updateStep3($id, array $data);
-    public function completeStep4($id);
     public function findById($id);
 }
