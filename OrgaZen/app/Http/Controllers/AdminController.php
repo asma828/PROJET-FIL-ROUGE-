3a3681f5c9be829081dbed23d76acf9c5181cad5
asma828 <asmalachhab66@gmail.com>
2025-04-22 11:07:15 +0100

    pass the roles with the view to display it

--- OrgaZen/app/Http/Controllers/AdminController.php
+++ OrgaZen/app/Http/Controllers/AdminController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\EventCategory;
+use App\Repositories\Interfaces\UserInterface;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    protected $userRepo;
+public function __construct(UserInterface $userRepo){
+    $this->userRepo=$userRepo;
+}
     public function index(){
         return view('components.admin.dashboard');
     }
 
     public function usersManagement(){
-        return view('components.admin.UserManagement');
+        $users=$this->userRepo->getAllUsers();
+        return view('components.admin.UserManagement',compact('users'));
     }
 
     public function events(){
