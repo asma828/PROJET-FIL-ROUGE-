6ac1c04a38535fc722f8c59b0461f635175e3386
asma828 <asmalachhab66@gmail.com>
2025-04-22 09:36:32 +0100

    add payement function to pass the paiement to stripe account

---
+++ OrgaZen/app/Http/Controllers/PaymentController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\reservation;
+use Illuminate\Http\Request;
+use Stripe\Stripe;
+use App\Models\Service;
+use Stripe\PaymentIntent;
+use Stripe\Checkout\Session;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Log;
+
+
+class PaymentController extends Controller
+{
+    
+  
+    
+    public function makePayment(Request $request)
+{    
+    Stripe::setApiKey(env('STRIPE_SECRET'));
+
+    $reservation = Reservation::findOrFail($request->reservation_id);
+    $service = Service::where('provider_id', $reservation->provider_id)->first();
+
+    $amount = max(500, ($service->price / $service->guest_count) * $reservation->guest_count * 100); 
+
+
+    try {
+        $payment = PaymentIntent::create([
+            'amount' => $amount,
+            'currency' => 'usd',
+            'payment_method' => $request->payment_method,
+            'confirm' => true,
+            'automatic_payment_methods' => [
+                'enabled' => true,
+                'allow_redirects' => 'never',
+            ],
+        ]);
+    
+
+$reservation->update([
+            'status' => 'completed',
+            'is_paid' => true,
+        ]);
+        return redirect()->route('components.client.EventHistory')->with('success', 'Payment successful!');
+    } catch (\Exception $e) {
+        \Log::error(' Stripe error: ' . $e->getMessage());
+        return back()->withErrors('Stripe error: ' . $e->getMessage());
+    }
+}
+
+    
+}
+
