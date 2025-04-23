e53e2ac0ee00bf677324670491d7b1432e7f299b
asma828 <asmalachhab66@gmail.com>
2025-04-23 14:34:33 +0100

    implement function to getClientEvents

--- OrgaZen/app/Repositories/ReservationRepository.php
+++ OrgaZen/app/Repositories/ReservationRepository.php
@@ -2,6 +2,7 @@
 namespace App\Repositories;
 
 use App\Models\reservation;
+use App\Models\User;
 use App\Repositories\Interfaces\ReservationInterface;
 
 class ReservationRepository implements ReservationInterface
@@ -23,5 +24,11 @@ class ReservationRepository implements ReservationInterface
     public function destroy($id){
         $user = reservation::findOrFail($id);
         return $user->delete();
+    }
+
+    public function getClientEvents($clientId)
+    {
+        $client = User::with(['events.category'])->findOrFail($clientId);
+        return $client;
     }
 }
