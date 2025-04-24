99a52e2ea103ae1f9bf0e81473af21265374917b
asma828 <asmalachhab66@gmail.com>
2025-04-24 10:56:10 +0100

    pass reservation and comment to the provider dashboard

--- OrgaZen/app/Http/Controllers/ProviderController.php
+++ OrgaZen/app/Http/Controllers/ProviderController.php
@@ -2,21 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\Interfaces\UserInterface;
 use Illuminate\Http\Request;
 
 class ProviderController extends Controller
 {
-    public function dashboard(){
-        return view('components.provider.dashboard');
+    protected $userRepository;
+
+    public function __construct(UserInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
     }
 
+     public function dashboard($providerId)
+    {
+        $provider = $this->userRepository->getProviderDashboardData($providerId);
+
+        return view('components.provider.dashboard', [
+            'provider' => $provider['provider'],
+            'latestReservation' => $provider['latestReservation'],
+            'latestComment' => $provider['latestComment'],
+        ]);
+    }
+    
     public function chat(){
         return view('components.provider.LiveChat');
     }
 
-    public function Booking(){
-        return view('components.provider.BookingManagement');
-    }
 
     
 }
