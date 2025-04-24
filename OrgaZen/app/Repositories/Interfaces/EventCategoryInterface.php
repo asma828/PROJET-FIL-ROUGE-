cc4081893d3ca11f456a45fcd46e251894f2f94c
asma828 <asmalachhab66@gmail.com>
2025-04-24 16:04:57 +0100

    add a function to find and update category

--- OrgaZen/app/Repositories/Interfaces/EventCategoryInterface.php
+++ OrgaZen/app/Repositories/Interfaces/EventCategoryInterface.php
@@ -7,5 +7,6 @@ interface EventCategoryInterface
     public function getAllCategory();
     public function destroy($id);
     public function store(array $data);
-  
+    public function update($id, array $data);
+    public function findById($id);
 }
