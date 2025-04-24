464362c3631cc171a98749088a67152fa4127541
asma828 <asmalachhab66@gmail.com>
2025-04-24 16:48:25 +0100

    add fillable to event category

--- OrgaZen/app/Models/EventCategory.php
+++ OrgaZen/app/Models/EventCategory.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class EventCategory extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'description', 'image'];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
