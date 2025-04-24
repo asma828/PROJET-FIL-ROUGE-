ae6a851b0471b02e7895eb6c0bf5c4c0609910bc
asma828 <asmalachhab66@gmail.com>
2025-04-24 16:50:55 +0100

    implement statistiques functions

--- OrgaZen/app/Repositories/StatistiqueRepository.php
+++ OrgaZen/app/Repositories/StatistiqueRepository.php
@@ -1,10 +1,22 @@
 <?php
 namespace App\Repositories;
 use App\Models\reservation;
+use App\Models\User;
 use App\Repositories\Interfaces\StatistiqueInterface;
 
 class StatistiqueRepository implements StatistiqueInterface{
     public function getTotalRevenus(){
         return Reservation::sum('total_price');
+    }
+    public function getTotalReservations(){
+        return reservation::count();
+    }
+    public function getTotalUsers(){
+        return User::count();
+    }
+    public function getTotalProvider(){
+        return User::with('role')
+        ->where('role_id',3)
+        ->count();
     }
 }
