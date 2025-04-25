e15de7ac9dd5697a24f7f225c031e5d7168ddd3f
asma828 <asmalachhab66@gmail.com>
2025-04-25 12:07:51 +0100

    create find and update function in tag interface

--- OrgaZen/app/Repositories/Interfaces/TagInterface.php
+++ OrgaZen/app/Repositories/Interfaces/TagInterface.php
@@ -6,4 +6,6 @@ interface TagInterface{
 public function getAllTags();
 public function destroy($id);
 public function store(array $data);
+public function update($id, array $data);
+public function findById($id);
 }
