7718a81a3919516aaa62440defb756793cf74b97
asma828 <asmalachhab66@gmail.com>
2025-04-29 09:18:02 +0100

    display the statistiques

--- OrgaZen/resources/views/components/admin/EventManagement.blade.php
+++ OrgaZen/resources/views/components/admin/EventManagement.blade.php
@@ -150,7 +150,7 @@
                             </div>
                             <div>
                                 <h3 class="text-lg font-semibold text-gray-700">Upcoming Events</h3>
-                                <p class="text-2xl font-bold text-indigo-600">0</p>
+                                <p class="text-2xl font-bold text-indigo-600">{{$Upcoming}}</p>
                             </div>
                         </div>
                     </div>
@@ -162,7 +162,7 @@
                             </div>
                             <div>
                                 <h3 class="text-lg font-semibold text-gray-700">Completed</h3>
-                                <p class="text-2xl font-bold text-purple-600">0</p>
+                                <p class="text-2xl font-bold text-purple-600">{{$CompleteReservation}}</p>
                             </div>
                         </div>
                     </div>
